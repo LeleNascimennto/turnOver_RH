65a68d1e14b6496ccecfca20ca264df29761ef53
Leticia Nasciemento <leticia.nascimento@germinare.org.br>
2026-09-25 09:48:23 -0300

    Fix

--- treinar.py
+++ treinar.py
@@ -146,7 +146,6 @@ def main():
             signature=infer_signature(X, modelo.predict(X)),
             input_example=X.head(3),
             registered_model_name=config.NOME_MODELO,
-            skops_trusted_types=["sklearn.tree._tree.Tree"],
         )
         run_id = run.info.run_id
 
